7523f0f1b4ae3e9455c9b87ba86b9209bdb3eae3
padmajasiva <padmajasiva09@gmail.com>
2026-07-02 21:49:45 +0530

    Added largest of two numbers

---
+++ Decision-Making/largest_two.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 20;
+    int b = 35;
+
+    if(a > b)
+        printf("%d is largest",a);
+    else
+        printf("%d is largest",b);
+
+    return 0;
+}
