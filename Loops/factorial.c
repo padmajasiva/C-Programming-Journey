c23ac3dbca2fc1ca0af460e1ceb6b95b40f4354f
padmajasiva <padmajasiva09@gmail.com>
2026-07-03 23:42:36 +0530

    Added factorial program

---
+++ Loops/factorial.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+    int i, fact = 1;
+
+    for(i = 1; i <= n; i++)
+    {
+        fact *= i;
+    }
+
+    printf("Factorial = %d", fact);
+
+    return 0;
+}
